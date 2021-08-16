26b869b56a140f19e56023617bde9fbd429edca7
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-08-16 02:37:32 -0300

    Series page added

---
+++ src/pages/Series.vue
@@ -0,0 +1,7 @@
+<template>Series</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('src/pages/HomePage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/HomePage.vue') },
+      { path: '/series/:id', component: () => import('src/pages/Series.vue') },
+    ],
   },
 
   // Always leave this as last one,
