31fc1051a7b1fb89b75782cb4353e926edad77d5
João Lucas de Oliveira Lopes <jlucaso@hotmail.com>
2021-08-16 02:37:22 -0300

    Footer added

---
+++ src/components/layout/CFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer class="footer">
+    <container>
+      <div class="flex wrap justify-between">
+        <div class="list">
+          <div class="text-h6">Navegação</div>
+          <router-link to="/">Séries Populares</router-link>
+          <router-link to="/">Séries em Simulcast</router-link>
+          <router-link to="/">Ler Mangá</router-link>
+          <router-link to="/">Calendário de Lançamentos</router-link>
+          <router-link to="/">Originals</router-link>
+          <router-link to="/">Notícias</router-link>
+          <router-link to="/">Fórum</router-link>
+          <router-link to="/">Jogos</router-link>
+        </div>
+        <div class="list">
+          <div class="text-h6">Contate-nos</div>
+          <router-link to="/">Youtube</router-link>
+          <router-link to="/">Facebook</router-link>
+          <router-link to="/">Twitter</router-link>
+          <router-link to="/">Instagram</router-link>
+        </div>
+      </div>
+    </container>
+  </footer>
+</template>
+
+<script>
+import Container from './Container.vue';
+export default {
+  components: {
+    Container,
+  },
+};
+</script>
+
+<style>
+.foo Containerter {
+  background: #000 linear-gradient(180deg, #0000, #213944);
+  position: relative;
+}
+.list > * {
+  display: flex;
+  color: white;
+  text-decoration: none;
+}
+.sections {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,15 +4,18 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <c-footer />
   </q-layout>
 </template>
 
 <script lang="ts">
 import Navbar from 'src/components/layout/Navbar.vue';
+import CFooter from 'src/components/layout/CFooter.vue';
 
 export default {
   components: {
     Navbar,
+    CFooter,
   },
 };
 </script>
